ecf391d246acec1c590c53f6355b5ce7e5a58dc0
Shameen-ghyas <shameenghyas@gmail.com>
2025-11-01 11:31:38 +0500

    removed socket.io

--- roll-connect-backend/index.js
+++ roll-connect-backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const connectDB = require('./config/db');   
 const cors = require('cors');
 require('dotenv').config();
@@ -9,7 +8,7 @@ const gameRoutes = require('./routes/gameRoutes.js');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {cors: {origin: "*"}});
+
 
 app.use(cors());
 app.use(express.json());
@@ -23,15 +22,8 @@ app.use('/api/game', gameRoutes);
 
 app.get('/', (req, res) => {
     res.send('Roll Connect Backend is running');
-})
-
-io.on('connection', (socket) => {
-   console.log(`New Player connected:", ${socket.id}`);
-
-   socket.on('disconnect', () => {
-    console.log(`Player disconnected: ${socket.id}`);
-   });
 });
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
